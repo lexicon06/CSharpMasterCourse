251c6c90982424353813c625d0ca497afbda1704
lexicon06 <pablo.hehe2@gmail.com>
2023-12-26 22:56:31 -0300

    Converted few things

---
+++ VariablesTC/TypeConversions/Program.cs
@@ -0,0 +1,20 @@
+﻿int personAge;
+
+Console.WriteLine("What Age is it?");
+string? ageText = Console.ReadLine();
+
+while(!int.TryParse(ageText, out personAge))
+{
+    Console.WriteLine("Please enter a valid age!");
+    ageText = Console.ReadLine();
+}
+
+
+Console.WriteLine($"Person is {personAge} Years Old");
+
+
+double testDouble = personAge;
+
+decimal testDecimal = (decimal)testDouble;//casting conversion
+
+Console.WriteLine($"{testDouble} vs {testDecimal}");
