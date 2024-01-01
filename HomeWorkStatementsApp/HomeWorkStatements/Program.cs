8c2dbc5abda1a13da51527c24cd576a5d1841870
lexicon06 <pablo.hehe2@gmail.com>
2024-01-01 16:08:20 -0300

    Succesfully created the homework for the statements

---
+++ HomeWorkStatementsApp/HomeWorkStatements/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+
+Console.WriteLine("Please enter your first name:");
+
+
+string? userName = Console.ReadLine();
+
+
+
+while (userName == null || userName == "")
+{
+    Console.WriteLine("Please enter a valid name");
+    userName = Console.ReadLine();
+}
+
+if (userName.ToLower() == "pablo")
+{
+    Console.WriteLine($"Welcome professor {userName}");
+}
+else
+{
+    Console.WriteLine($"Welcome student {userName}");
+}
