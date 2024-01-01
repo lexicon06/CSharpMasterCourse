a2f53276ee022bff5b911b34e58721426bed11c0
lexicon06 <pablo.hehe2@gmail.com>
2024-01-01 15:42:19 -0300

    Advanced if statements

---
+++ AdvanceIf/AdvanceIf/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+Console.WriteLine("What's your name?");
+string? firstName = Console.ReadLine();
+Console.WriteLine("What's your last name?");
+string? lastName = Console.ReadLine();
+
+
+if (firstName?.ToLower() == "pablo" && lastName?.ToLower() == "santillan")
+{
+    Console.WriteLine("----------------------------");
+    Console.WriteLine($"Hello Mr.{lastName}");
+    Console.WriteLine("----------------------------");
+}
+else if (firstName?.ToLower() == "pablo" || lastName?.ToLower() == "santillan")
+{
+
+    Console.WriteLine("----------------------------");
+    Console.WriteLine($"Hello Fox");
+    Console.WriteLine("----------------------------");
+}
+else
+{
+    Console.WriteLine("----------------------------");
+    Console.WriteLine($"Hello {firstName}");
+    Console.WriteLine("----------------------------");
+}
