aa3fbb1aa2623b895977ce44bbb8b0a9b9620046
lexicon06 <pablo.hehe2@gmail.com>
2024-01-01 18:37:41 -0300

    Finish the homework

---
+++ HomeworkDoLoopApplication/HomeworkDoLoop/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+Console.WriteLine("Welcome to homework app: do while loop");
+
+string? userName = null;
+
+while (userName == String.Empty || userName == null)
+{
+
+    Console.WriteLine("Please enter your name:");
+    userName = Console.ReadLine();
+
+}
+
+
+string inputText = "";
+
+do
+{
+    if (userName.ToLower() == "fox")
+    {
+        Console.WriteLine("Welcome professor Pablo");
+    }
+    else
+    {
+        Console.WriteLine("Welcome student");
+    }
+
+    Console.WriteLine("""Please type "exit" to terminate the program.""");
+
+    inputText = Console.ReadLine();
+} while (inputText != "exit");
