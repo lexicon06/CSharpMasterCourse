4e220dc4656ffb97c4350697af43aa8ff48984d4
lexicon06 <pablo.hehe2@gmail.com>
2024-01-01 18:02:17 -0300

    Homework done.

---
+++ HomeWorkApp/HomeWorkClassStart/Program.cs
@@ -0,0 +1,56 @@
+﻿
+
+float version = 1.0f;
+
+bool isProfessor = false;
+
+Console.WriteLine($"Console HOMEWORK application v{version.ToString("N1").Replace(",", ".")} for the course");
+
+Console.WriteLine("Please enter your name:");
+
+string? userName = Console.ReadLine();
+
+
+while (userName == null || userName == "")
+{
+    Console.WriteLine("Please enter a valid name");
+    userName = Console.ReadLine();
+}
+
+Console.WriteLine("Please enter your age:");
+
+int userAge;
+
+while (!int.TryParse(Console.ReadLine(), out userAge))
+{
+    Console.WriteLine("Please enter a valid Age");
+}
+
+if (userName.ToLower() == "bob" || userName.ToLower() == "sue")
+{
+    Console.WriteLine($"Welcome professor {userName}");
+    isProfessor = true;
+}
+
+else
+{
+    Console.WriteLine($"Welcome user {userName}!");
+}
+
+
+switch (userAge)
+{
+    case < 21:
+        Console.WriteLine($"Sorry {userName} we apologize, but it's recommended that you have at least 21yo, to participate in this class.");
+        break;
+    default:
+        if (!isProfessor) { Console.WriteLine($"It's time for your class {userName}."); }
+        else
+        {
+            Console.WriteLine($"Let's go professor {userName}.");
+        }
+        break;
+}
+
+
+
