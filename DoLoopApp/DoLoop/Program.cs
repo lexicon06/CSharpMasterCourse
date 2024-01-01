8701ef07c02e6744ba01496b1c0d2a9e6cc64d79
lexicon06 <pablo.hehe2@gmail.com>
2024-01-01 18:27:36 -0300

    do loop completed

---
+++ DoLoopApp/DoLoop/Program.cs
@@ -0,0 +1,30 @@
+﻿
+bool isValidAge;
+int userAge;
+
+
+do
+{
+    Console.WriteLine("What is your age?:");
+    string ageText = Console.ReadLine();
+
+    isValidAge = int.TryParse(ageText, out userAge);
+
+    if (!isValidAge)
+    {
+        Console.WriteLine("That was an invalid age.");
+    }
+} while (!isValidAge);
+
+
+
+Console.WriteLine($"Your age is {userAge}");
+
+Console.WriteLine("Lets calc together with a loop how much need to reach 100");
+
+int counter = 0;
+while(userAge++ < 100)
+{
+    counter++;
+    Console.WriteLine($"user age is increasing {userAge}, {counter} times");
+}
