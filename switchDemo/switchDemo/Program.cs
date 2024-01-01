cc599d88bdbae8616c9b51c111b33c61a14dac36
lexicon06 <pablo.hehe2@gmail.com>
2024-01-01 16:02:36 -0300

    switch in c#

---
+++ switchDemo/switchDemo/Program.cs
@@ -0,0 +1,52 @@
+﻿//string? password = "abc544648@€&$";
+
+
+//Console.WriteLine("Please enter your name");
+//string? userName = Console.ReadLine();
+
+int ageNumber;
+
+Console.WriteLine("Hey, enter your age:");
+
+
+while (!int.TryParse(Console.ReadLine(), out ageNumber))
+{
+    Console.WriteLine("Please enter a valid number");
+}
+
+
+switch (ageNumber)
+{
+    case > 12 and < 20:
+        Console.WriteLine("You are a teenager");
+        break;
+    case >= 20:
+        Console.WriteLine("You are an adult");
+        break;
+    default:
+        Console.WriteLine("You are a baby");
+        break;
+}
+
+
+/*
+
+switch (userName?.ToLower())
+{
+    case "fox":
+    case "pablo" or "santillan":
+        {
+            Console.WriteLine($"Hey Fox, your password is {password}");
+            break;
+        }
+       
+
+    default:
+        {
+            Console.WriteLine($"Welcome {userName}");
+            break;
+        }
+       
+}
+
+ */
