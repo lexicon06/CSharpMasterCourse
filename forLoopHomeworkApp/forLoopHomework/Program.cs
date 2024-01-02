dbe375c5147cbe067e922ac369ab913099c64a66
lexicon06 <pablo.hehe2@gmail.com>
2024-01-02 04:59:15 -0300

    completed list for loop, foreach homework

---
+++ forLoopHomeworkApp/forLoopHomework/Program.cs
@@ -0,0 +1,19 @@
+﻿
+//TODO add more checks
+
+string names = "";
+var usersList = new List<string>();
+do
+{
+    Console.WriteLine("Please enter users list separated with a comma and no space");
+    Console.WriteLine("jake,patrick,louis,mario,gero");
+    names = Console.ReadLine();
+} while (names == "" || names.Length == 0 || !names.Contains(",") && names.Contains(" "));
+
+usersList = names.Split(",").ToList();
+
+
+foreach(string user in usersList)
+{
+    Console.WriteLine($"Welcome {user}");
+}
