b7752e41aca5a33acc16877bd193ed899c33b3a4
lexicon06 <pablo.hehe2@gmail.com>
2024-01-02 04:31:07 -0300

    Dictionaries homework complete

---
+++ DictionariesHomeworkApp/DictionariesHomework/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+//Dictionaries homework
+
+
+var employeeIds = new Dictionary<int, string>();
+
+employeeIds.Add(1, "Paul");
+employeeIds.Add(2, "Jack");
+employeeIds.Add(3, "Bill");
+employeeIds.Add(4, "Marty");
+employeeIds.Add(5, "Michael");
+
+
+Console.WriteLine($"Please enter your employee ID its between 1/{employeeIds.Count}");
+
+int userIdInput = 0;
+
+while (!int.TryParse(Console.ReadLine(), out userIdInput) || userIdInput < 1 || userIdInput > employeeIds.Count)
+{
+    Console.WriteLine("Please enter a valid ID!!!");
+}
+
+Console.WriteLine($"Welcome back {employeeIds[userIdInput]}");
