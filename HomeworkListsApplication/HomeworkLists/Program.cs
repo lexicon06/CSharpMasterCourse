3ac7915d558985cb6904d9060b66aa81fe439551
lexicon06 <pablo.hehe2@gmail.com>
2024-01-02 02:59:38 -0300

    list homework completed 😁

---
+++ HomeworkListsApplication/HomeworkLists/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+
+using System;
+
+List<string> students = new List<string>();
+
+
+string typed = "";
+
+do
+{
+    Console.WriteLine("Please enter the new student name:");
+    typed = Console.ReadLine();
+    if (typed != "done" && typed != "")
+    {
+        students.Add(typed);
+        Console.WriteLine("====================");
+        Console.WriteLine("Students List:");
+        foreach (string s in students)
+        {
+            Console.WriteLine($"Name: {s}");
+        }
+
+        Console.WriteLine("\nType 'done' to exit the program.\n");
+    }
+    else if (typed == "done")
+    {
+        return;
+    }
+}
+while (typed != "done");
