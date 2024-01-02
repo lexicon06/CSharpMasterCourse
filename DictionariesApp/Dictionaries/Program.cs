a147472e8a3fae6f44757eceb8bc5f2692c36843
lexicon06 <pablo.hehe2@gmail.com>
2024-01-02 04:24:36 -0300

    Dictionaries app

---
+++ DictionariesApp/Dictionaries/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+var dictionaryEmployee = new Dictionary<int, string>();
+
+dictionaryEmployee.Add(1, "Lucas");
+dictionaryEmployee.Add(2, "Patrick");
+dictionaryEmployee[4] = "Ricardo";
+
+
+Console.WriteLine($"User {dictionaryEmployee[4]}");
+
+var dictionaryMovies = new Dictionary<string, string>();
+
+dictionaryMovies.Add("galaxy", "Galaxy movie is about aliens and space");
+dictionaryMovies.Add("gnome", "Gnome movie is about gnomes and zombies");
+
+Console.WriteLine($"Movie is {dictionaryMovies["gnome"]}");
