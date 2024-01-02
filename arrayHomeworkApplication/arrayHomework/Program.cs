eb0aab724c654af4cf4a21ca4b71b4204d5ae92f
lexicon06 <pablo.hehe2@gmail.com>
2024-01-02 00:47:36 -0300

    arrays Homework done

---
+++ arrayHomeworkApplication/arrayHomework/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+
+string[] userNames = new string[] { "Jacob", "Alan", "Gabriel" };
+
+Console.WriteLine("Please select a number between 1 to 3");
+
+int userNumber = 0;
+
+while (!int.TryParse(Console.ReadLine(), out userNumber) || userNumber < 1 || userNumber > 3)
+{
+    Console.WriteLine("Please enter a valid number between 1 and 3");
+}
+
+Console.WriteLine($"You have selected: {userNames[userNumber - 1]}");
+
+Console.WriteLine("Good job!");
+
