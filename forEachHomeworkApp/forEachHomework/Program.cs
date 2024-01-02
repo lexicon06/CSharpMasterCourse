e1c8b9d1535211a0fae396533945f583cdd92491
lexicon06 <pablo.hehe2@gmail.com>
2024-01-02 05:13:23 -0300

    foreachhomework done

---
+++ forEachHomeworkApp/forEachHomework/Program.cs
@@ -0,0 +1,44 @@
+﻿
+
+string firstName = "";
+
+bool isComplete = false;
+
+var userNames = new List<string>();
+
+bool isValidName(string name)
+{
+    if (name.Length >= 3 && name.ToLower() != "exit")
+    {
+        return true;
+    }
+    return false;
+}
+
+do
+{
+    Console.WriteLine("Please enter your first name (type exit to quit)");
+    firstName = Console.ReadLine();
+    if (isValidName(firstName))
+    {
+        userNames.Add(firstName);
+    }
+    else if (firstName.ToLower() == "exit")
+    {
+        isComplete = true;
+    }
+
+    if (userNames.Count > 0)
+    {
+        foreach (string name in userNames)
+        {
+            Console.WriteLine($"Hello {name}");
+        }
+    }
+
+}
+while (!isComplete);
+
+
+
+Console.WriteLine("Goodbye!");
