2261be77c27c7ff3c0fd41b0b7b6c949a6a804aa
lexicon06 <pablo.hehe2@gmail.com>
2024-01-03 17:16:35 -0300

    Homework done, methods.

---
+++ HomeworkMethodsApplication/HomeworkMethods/Program.cs
@@ -0,0 +1,14 @@
+﻿using HomeworkMethods;
+
+
+Console.WriteLine("###############################");
+Console.WriteLine("###############################");
+Console.WriteLine("Homework application 'methods'");
+Console.WriteLine("###############################");
+Console.WriteLine("###############################");
+
+
+string userName = ConsoleHomeworkMethod.userWelcome();
+
+ConsoleHomeworkMethod.SayHi(userName);
+
---
+++ HomeworkMethodsApplication/HomeworkMethods/WelcomeMethod.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkMethods
+{
+    public class ConsoleHomeworkMethod
+    {
+        public static string userWelcome()
+        {
+            Console.WriteLine("Hello, please enter your name");
+
+            string userName = Console.ReadLine();
+
+            return userName;
+        }
+
+        public static void SayHi(string name)
+        {
+            Console.WriteLine($"Hello {name}");
+        }
+    }
+}
