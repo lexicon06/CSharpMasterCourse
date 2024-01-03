85a25e32b013c7591243a287100ef94f851030f1
lexicon06 <pablo.hehe2@gmail.com>
2024-01-03 14:03:49 -0300

    Added few methods more

--- MethodsApp/Methods/Program.cs
+++ MethodsApp/Methods/Program.cs
@@ -8,3 +8,10 @@ Console.WriteLine("----GOODBYE----");
 ConsoleGreeting.SayGoodbye("Pablo");
 Console.WriteLine("--------");
 ConsoleGreeting.SayGoodbye("Fox");
+
+
+mathShortcuts.Add(1, 1);
+
+double[] numbers = new double[] { 2, 2, 2 };
+
+mathShortcuts.addAll(numbers);
