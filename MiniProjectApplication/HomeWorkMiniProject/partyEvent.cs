0c47d15109379b649675cb50b6262f2ca186750c
lexicon06 <pablo.hehe2@gmail.com>
2024-01-03 18:13:58 -0300

    Homework completed mini project using methods a variety of skills

---
+++ MiniProjectApplication/HomeWorkMiniProject/partyEvent.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkMiniProject
+{
+    public class partyEvent
+    {
+        public static int totalGuest = 0;
+        public static (string, int) GuestBook()
+        {
+            Console.WriteLine("Please enter your guest name");
+            string guestName = Console.ReadLine();
+
+            int guestFamily = 0;
+
+            Console.WriteLine("Enter how many for this family:");
+
+            while (!int.TryParse(Console.ReadLine(), out guestFamily) || guestFamily == 0)
+            {
+                Console.WriteLine("Please enter a valid number of members");
+            }
+
+            return (guestName, guestFamily);
+        }
+
+        public static int guestList(string familyName, int howMany)
+        {
+            totalGuest += howMany;
+
+            Console.WriteLine($"Family: {familyName} | Members: {howMany}");
+
+            return totalGuest;
+        }
+
+
+        public static void resetList()
+        {
+            totalGuest = 0;
+        }
+    }
+}
