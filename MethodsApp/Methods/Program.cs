f74a2987e2a2351cef172190726886d3d5b28c6f
lexicon06 <pablo.hehe2@gmail.com>
2024-01-03 02:54:53 -0300

    MethodsApp

---
+++ MethodsApp/Methods/Program.cs
@@ -0,0 +1,10 @@
+﻿using Methods;
+
+Console.WriteLine("----WELCOME----");
+ConsoleGreeting.SayHi("Pablo");
+Console.WriteLine("--------");
+ConsoleGreeting.SayHi("Fox");
+Console.WriteLine("----GOODBYE----");
+ConsoleGreeting.SayGoodbye("Pablo");
+Console.WriteLine("--------");
+ConsoleGreeting.SayGoodbye("Fox");
---
+++ MethodsApp/Methods/SampleMethods.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace Methods
+//{
+//    internal static class SampleMethods
+//    {
+//        internal static void Main(string[] args)
+//        {
+//            Console.WriteLine("Hello buddy");
+//        }
+//    }
+//}   the universal way in earlier versions
+
+
+
+
+
+namespace Methods
+{
+    public static class ConsoleGreeting
+    {
+        public static void SayHi(string name)
+        {
+            Console.WriteLine($"Hi {name}");
+        }
+
+        public static void SayGoodbye(string name)
+        {
+            Console.WriteLine($"Goodbye {name}");
+        }
+    }
+}
