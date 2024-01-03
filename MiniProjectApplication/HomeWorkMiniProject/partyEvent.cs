4a80b00ea6cbdc001ff02b4e828a18446b11eed9
lexicon06 <pablo.hehe2@gmail.com>
2024-01-03 19:13:10 -0300

    improved the logic to a better structure

--- MiniProjectApplication/HomeWorkMiniProject/partyEvent.cs
+++ MiniProjectApplication/HomeWorkMiniProject/partyEvent.cs
@@ -8,37 +8,89 @@ namespace HomeWorkMiniProject
 {
     public class partyEvent
     {
-        public static int totalGuest = 0;
-        public static (string, int) GuestBook()
+        public static Dictionary<string, int> peopleInvited = new Dictionary<string, int>();
+        public static void WelcomeApp()
         {
-            Console.WriteLine("Please enter your guest name");
-            string guestName = Console.ReadLine();
+            Console.WriteLine("**********************************");
+            Console.WriteLine("Welcome to Party Event Application");
+            Console.WriteLine("**********************************");
+        }
 
-            int guestFamily = 0;
+        internal static bool keepCollecting()
+        {
+            Console.WriteLine("Is there more to add yes/no");
+            bool userInput = Console.ReadLine() == "yes";
+            return userInput;
+        }
 
-            Console.WriteLine("Enter how many for this family:");
 
-            while (!int.TryParse(Console.ReadLine(), out guestFamily) || guestFamily == 0)
+        public static void CollectPeople()
+        {
+            do
             {
-                Console.WriteLine("Please enter a valid number of members");
+                Console.WriteLine("Who is the family?: ");
+                string familyName = Console.ReadLine();
+                Console.WriteLine("How many members?: ");
+                int familyUsers = 0;
+                while (!int.TryParse(Console.ReadLine(), out familyUsers))
+                {
+                    Console.WriteLine("Please enter a valid number");
+                }
+                if (peopleInvited.ContainsKey(familyName))
+                {
+                    Console.WriteLine("Sorry, the family already is in the List");
+                }
+                else
+                {
+                    peopleInvited.Add(familyName, familyUsers);
+                }
+            }
+            while (keepCollecting());
+        }
+
+        public static void DisplayCollection()
+        {
+            Console.WriteLine("\n######################");
+            Console.WriteLine("#####GUEST LIST#######");
+            Console.WriteLine("######################\n");
+
+            foreach (var person in peopleInvited.Keys)
+            {
+                Console.WriteLine($"Family: {person} - Members: {peopleInvited[person]}");
             }
 
-            return (guestName, guestFamily);
         }
+        //public static int totalGuest = 0;
+        //public static (string, int) GuestBook()
+        //{
+        //    Console.WriteLine("Please enter your guest name");
+        //    string guestName = Console.ReadLine();
 
-        public static int guestList(string familyName, int howMany)
-        {
-            totalGuest += howMany;
+        //    int guestFamily = 0;
 
-            Console.WriteLine($"Family: {familyName} | Members: {howMany}");
+        //    Console.WriteLine("Enter how many for this family:");
 
-            return totalGuest;
-        }
+        //    while (!int.TryParse(Console.ReadLine(), out guestFamily) || guestFamily == 0)
+        //    {
+        //        Console.WriteLine("Please enter a valid number of members");
+        //    }
+
+        //    return (guestName, guestFamily);
+        //}
+
+        //public static int guestList(string familyName, int howMany)
+        //{
+        //    totalGuest += howMany;
+
+        //    Console.WriteLine($"Family: {familyName} | Members: {howMany}");
+
+        //    return totalGuest;
+        //}
 
 
-        public static void resetList()
-        {
-            totalGuest = 0;
-        }
+        //public static void resetList()
+        //{
+        //    totalGuest = 0;
+        //}
     }
 }
