4e6356b2460434222f3736d6787a87c71466d48b
lexicon06 <pablo.hehe2@gmail.com>
2024-01-03 21:57:43 -0300

    Error debugging with error handling

---
+++ ExceptionHandlingDemo/ConsoleUI/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ages = new int[] { 1, 4, 5 };
+            for (int i = 0; i <= ages.Length; i++)
+            {
+                try
+                {
+                    Console.WriteLine(ages[i]);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Sorry we had an error " + ex.Message);
+                }
+            }
+            Console.WriteLine("hello from netcore app");
+            Console.ReadLine();
+        }
+    }
+}
