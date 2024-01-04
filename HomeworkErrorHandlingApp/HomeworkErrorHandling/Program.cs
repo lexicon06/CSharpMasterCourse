10c2994e4516eea5d8cc159bdf3cc3f26398bc9f
lexicon06 <pablo.hehe2@gmail.com>
2024-01-03 22:31:34 -0300

    Homework done

---
+++ HomeworkErrorHandlingApp/HomeworkErrorHandling/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkErrorHandling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Error handling App Demo");
+
+            try
+            {
+                Bug();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error Bug() ", ex);
+            }
+
+
+
+            Console.ReadLine();
+
+            return;
+        }
+
+
+        private static void Bug()
+        {
+            int[] arr = new int[5];//empty arr
+
+            for (int i = 0; i <= arr.Length; i++)
+            {
+                try
+                {
+                    Console.WriteLine(arr[i]);
+                }
+
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Something went wrong " + ex.Message);
+                    throw new Exception("There was an error handling the array", ex);
+
+                }
+            }
+        }
+    }
+}
