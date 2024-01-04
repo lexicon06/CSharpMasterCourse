b49ef1f5be36b8ffeb53c501ce9ba4a975124642
lexicon06 <pablo.hehe2@gmail.com>
2024-01-03 21:36:17 -0300

    debug app

---
+++ ConsoleUIApp/ConsoleUI/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+
+namespace ConsoleUI;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i < 20; i++)
+        {
+            Console.WriteLine($"The value of i is {i}");
+            for(int j = 0; j < 20; j++)//the bug was i++
+            {
+                Console.WriteLine($"The value of j is {j}");
+            }
+        }
+    }
+}
