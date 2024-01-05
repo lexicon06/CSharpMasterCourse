b58f22d66602223c0057b7d9df92af715d801fb8
lexicon06 <pablo.hehe2@gmail.com>
2024-01-05 11:26:11 -0300

    Classes Homework project

---
+++ StaticClassesDemo/ConsoleUI/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IntroMessage("#####ClassAPP####");
+            Console.Write("What is your name: ");
+            string userName = Console.ReadLine();
+            UserMessages.Welcome(userName);
+            Console.ReadLine();
+        }
+
+        private static void IntroMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ StaticClassesDemo/ConsoleUI/UserMessages.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    public class UserMessages
+    {
+        public static void Welcome(string userName)
+        {
+
+            Console.Clear();
+
+            int time = DateTime.Now.Hour;
+
+            if (time < 12)
+            {
+                GoodMorning(userName);
+            }
+            else if (time < 19)
+            {
+                GoodAfternoon(userName);
+            }
+            else
+            {
+                GoodEvening(userName);
+            }
+
+        }
+        internal static void GoodMorning(string userName)
+        {
+            Console.WriteLine($"Good morning {userName}");
+        }
+        internal static void GoodEvening(string userName)
+        {
+            Console.WriteLine($"Good evening {userName}");
+        }
+
+        internal static void GoodAfternoon(string userName)
+        {
+            Console.WriteLine($"Good afternoon {userName}");
+        }
+    }
+}
