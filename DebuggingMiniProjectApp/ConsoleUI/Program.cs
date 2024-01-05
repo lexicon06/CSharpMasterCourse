22dd4c0039950bae480871669b0a10899a02ec13
lexicon06 <pablo.hehe2@gmail.com>
2024-01-04 21:16:11 -0300

    Added the boiler plate code

--- DebuggingMiniProjectApp/ConsoleUI/Program.cs
+++ DebuggingMiniProjectApp/ConsoleUI/Program.cs
@@ -12,5 +12,42 @@ namespace ConsoleUI
         {
             Console.WriteLine(argumments);
         }
+
+        private static int W(int e)
+        {
+            int n = 1;
+            n = J(e) + 4;
+            return n;
+        }
+
+        private static int P(int y)
+        {
+            int o = 0;
+            o = F(y) + 2;
+            return 0;
+        }
+
+        private static int F(int x)
+        {
+            int y = 0;
+            y = x / 5;
+            return y;
+        }
+
+        private static int J(int a)
+        {
+            int c = 0;
+            a = a - 1;
+            c = R(a);
+            c = P(c) / 3;
+            return c;
+        }
+
+        private static int R(int v)
+        {
+            int z = 5;
+            z = 3 / v;
+            return v;
+        }
     }
 }
