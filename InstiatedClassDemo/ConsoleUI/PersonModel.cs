56c0aed42525e8b007c18dddae66aff5d25bb904
lexicon06 <pablo.hehe2@gmail.com>
2024-01-06 20:39:12 -0300

    Instantiated classes

---
+++ InstiatedClassDemo/ConsoleUI/PersonModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    public class PersonModel
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string emailAddress { get; set; }
+
+
+
+
+        
+    }
+}
---
+++ InstiatedClassDemo/ConsoleUI/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string firstName;
+            //string lastName;
+            //string emailAddress;
+
+            //string firstName2;
+            //string lastName2;
+            //string emailAddress2;
+
+            //List<string> firstNames = new List<string>();
+            //List<string> lastNames = new List<string>();
+
+
+            //PersonModel person = new PersonModel();
+
+            //Console.Write("Please enter that name: ");
+
+
+            //person.firstName = Console.ReadLine();
+
+
+            //Console.WriteLine($"Hello {person.firstName}");
+
+            //PersonModel person2 = new PersonModel();
+
+            //person2.firstName = "Jacob";
+
+            //Console.WriteLine($"Hello {person2.firstName}");
+
+
+            List<PersonModel> peopleGroup = new List<PersonModel>();
+
+            var guy = new PersonModel();
+
+            guy.firstName = "Pablo";
+
+            peopleGroup.Add(guy);
+
+
+            guy = new PersonModel();
+
+            guy.firstName = "Jack";
+
+            
+            peopleGroup.Add(guy);
+
+
+            foreach (var person in peopleGroup)
+            {
+                Console.WriteLine(person.firstName);
+            }
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
