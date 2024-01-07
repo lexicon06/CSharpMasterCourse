4fd51728e68af37aa424bc69a62ed2c68482a297
lexicon06 <pablo.hehe2@gmail.com>
2024-01-07 00:41:24 -0300

    Homework getters, setters, private full props

---
+++ HomeworkClassesGetSetPrivate/ClassesHomeworkGettersSettersPrivate/AdressModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesHomeworkGettersSettersPrivate
+{
+    public class AddressModel
+    {
+
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        private int _zipCode;
+
+        public int ZipCode
+        {
+            get { return _zipCode; }
+            set
+            {
+                if (value.ToString().Length < 4)
+                {
+                    throw new ArgumentException("value", "ZipCode is not correct");
+                }
+                else
+                {
+                    _zipCode = value;
+                }
+            }
+        }
+
+
+
+        public string FullAddress { get; private set; }
+
+
+        public AddressModel(string fullAdress)
+        {
+            FullAddress = fullAdress;
+        }
+
+    }
+}
---
+++ HomeworkClassesGetSetPrivate/ClassesHomeworkGettersSettersPrivate/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesHomeworkGettersSettersPrivate
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to homework app classes, getters, setters, private, full props");
+
+            Console.WriteLine("");
+
+            try
+            {
+                AddressModel Location = new AddressModel("Github 123123") { City = "Mar de Plata", State = "Buenos Aires", ZipCode = 7600 };
+
+                Console.WriteLine($"Living in {Location.FullAddress}, City {Location.City}, {Location.State}, ZipCode: {Location.ZipCode}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
