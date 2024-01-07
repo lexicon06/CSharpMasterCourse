f1578a0e36f6eb92f2c205301dd3bc64e2998141
lexicon06 <pablo.hehe2@gmail.com>
2024-01-07 06:14:43 -0300

    Class Library demo

---
+++ ClassLibrariesDemo/ConsoleUI/Program.cs
@@ -0,0 +1,21 @@
+﻿using DemoLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonModel person = new PersonModel();
+            person.FirstName = "Test";
+            person.LastName = "Test2";
+            Console.WriteLine($"Hello {person.FirstName}");
+            Console.ReadLine();
+        }
+    }
+}
