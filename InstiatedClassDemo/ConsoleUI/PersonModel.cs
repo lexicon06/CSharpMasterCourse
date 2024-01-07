7310c4c964ab5e05e80782d8e8ce69015ac9036a
lexicon06 <pablo.hehe2@gmail.com>
2024-01-06 21:08:59 -0300

    Played a bit with it to find out how really works

--- InstiatedClassDemo/ConsoleUI/PersonModel.cs
+++ InstiatedClassDemo/ConsoleUI/PersonModel.cs
@@ -8,13 +8,18 @@ namespace ConsoleUI
 {
     public class PersonModel
     {
-        public string firstName { get; set; }
-        public string lastName { get; set; }
-        public string emailAddress { get; set; }
+        //public string firstName { get; set; }
+        //public string lastName { get; set; }
+        //public string emailAddress { get; set; }
+
+        //the auto propertyy we type prop, tab to switch then edit each and enter
+
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
 
 
 
-
-        
     }
 }
--- InstiatedClassDemo/ConsoleUI/Program.cs
+++ InstiatedClassDemo/ConsoleUI/Program.cs
@@ -39,29 +39,69 @@ namespace ConsoleUI
             //Console.WriteLine($"Hello {person2.firstName}");
 
 
-            List<PersonModel> peopleGroup = new List<PersonModel>();
+            //List<PersonModel> peopleGroup = new List<PersonModel>();
 
-            var guy = new PersonModel();
+            //var guy = new PersonModel();
 
-            guy.firstName = "Pablo";
+            //guy.firstName = "Pablo";
 
-            peopleGroup.Add(guy);
-
-
-            guy = new PersonModel();
-
-            guy.firstName = "Jack";
-
-            
-            peopleGroup.Add(guy);
+            //peopleGroup.Add(guy);
 
 
-            foreach (var person in peopleGroup)
+            //guy = new PersonModel();
+
+            //guy.firstName = "Jack";
+
+
+            //peopleGroup.Add(guy);
+
+
+            //foreach (var person in peopleGroup)
+            //{
+            //    Console.WriteLine(person.firstName);
+            //}
+
+            //List<PersonModel> list = new List<PersonModel>();
+
+            //list.Add(new PersonModel { FirstName = "Jojo", LastName = "Joestar" });
+            //list.Add(new PersonModel { FirstName = "Pablo", LastName = "Santillan" });
+            //list.Add(new PersonModel { FirstName = "Freddy", LastName = "Kruger" });
+            //list.Add(new PersonModel { FirstName = "Goku", LastName = "Son" });
+            //list.Add(new PersonModel { FirstName = "Steve", LastName = "Jobs" });
+
+
+            //foreach (PersonModel model in list)
+            //{
+            //    Console.WriteLine(model.FirstName + " " + model.LastName);
+            //}
+
+
+            List<PersonModel> list = new List<PersonModel>();
+
+            string userName, lastName;
+            do
             {
-                Console.WriteLine(person.firstName);
+                Console.Write("Type a name or exit to terminate: ");
+                userName = Console.ReadLine();
+                if (userName != "exit")
+                {
+                    Console.WriteLine("What is your last name?");
+                    lastName = Console.ReadLine();
+                    list.Add(new PersonModel
+                    {
+                        FirstName = userName,
+                        LastName = lastName,
+                    });
+                }
+            } while (userName != "exit");
+
+
+            Console.WriteLine($"\nList of people ({list.Count}):\n");
+
+            foreach (PersonModel model in list)
+            {
+                Console.WriteLine(model.FirstName + " " + model.LastName);
             }
-
-
 
 
             Console.ReadLine();
