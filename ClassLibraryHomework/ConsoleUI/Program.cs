1bb03e951f047db7dde799a0ba2aa93c42f2affb
lexicon06 <pablo.hehe2@gmail.com>
2024-01-07 09:47:56 -0300

    Homework done created a class reference

---
+++ ClassLibraryHomework/ConsoleUI/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PersonLibrary;
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            PersonModel dev = new PersonModel("Fox");
+
+            dev.Stack = "JS, PHP, C#, NODEJS, REACT";
+
+            dev.Name = "Pablo";
+
+            dev.Bio = "A passionate programmer who likes code, music, games and i love my gf";
+
+            Console.WriteLine($"Great {dev.Name}");
+            Console.WriteLine($"{dev.Stack}, great tech stack");
+            Console.WriteLine($"{dev.Bio}, good work!");
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ ClassLibraryHomework/PersonLibrary/PersonModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonLibrary
+{
+    public class PersonModel
+    {
+        public string Name { get; set; }
+        public string Bio { get; set; }
+        public string Stack { get; set; }
+
+
+        public PersonModel(string name)
+        {
+            Console.WriteLine($"Welcome {name}");
+        }
+    }
+}
