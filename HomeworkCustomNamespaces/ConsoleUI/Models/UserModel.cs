fb025e7b41c19fcdc81f3399c4d143fa4ecdbdd5
lexicon06 <pablo.hehe2@gmail.com>
2024-01-07 05:41:27 -0300

    Homework namespaces

---
+++ HomeworkCustomNamespaces/ConsoleUI/Models/UserModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI.Models
+{
+    public class UserModel
+    {
+        public string UserName { private get; set; }
+        //public string Password { get; set; }
+        private string _password;
+
+        public string Password
+        {
+            get { return _password; }
+            set
+            {
+                if (value.Length > 6)
+                {
+                    _password = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("value", "Sorry, but you have to enter a pasword with more than 6 characters");
+                }
+
+            }
+        }
+
+        public string Email { get; set; }
+        public int UserAge { get; set; }
+
+        public void GreetUser()
+        {
+            Console.Write("Login Granted!");
+            Console.ReadLine();
+            Console.WriteLine($"Welcome {UserName}");
+        }
+    }
+}
---
+++ HomeworkCustomNamespaces/ConsoleUI/Program.cs
@@ -0,0 +1,42 @@
+﻿using ConsoleUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            UserModel person = new UserModel();
+            Console.WriteLine("Please enter your name");
+            string userName = Console.ReadLine();
+            person.UserName = userName;
+            int userAge;
+            do
+            {
+                Console.WriteLine("Please enter your age");
+            }
+            while (!int.TryParse(Console.ReadLine(), out userAge));
+
+            Console.WriteLine("Please enter your password");
+            try
+            {
+                string userPassword = Console.ReadLine();
+                person.Password = userPassword;
+                person.GreetUser();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            
+
+            Console.ReadLine();
+        }
+    }
+}
