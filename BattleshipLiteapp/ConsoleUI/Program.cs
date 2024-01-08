584a71d214e419ce6ad32b03b47a1892ef59ac01
lexicon06 <pablo.hehe2@gmail.com>
2024-01-08 05:07:46 -0300

    Battleship app in coming

---
+++ BattleshipLiteapp/ConsoleUI/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //TODO https://onedrive.live.com/view.aspx?resid=A97DBB57FDE9FA86%2116216&id=documents
+            Console.ReadLine();
+        }
+    }
+}
