aa4f2f269b8e02adcd3863beb228c553b464cfe0
lexicon06 <pablo.hehe2@gmail.com>
2024-01-10 05:49:48 -0300

    Added some basic methods and error handling for the game console ui

---
+++ BattleshipLiteAppDemo/ConsoleUI/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string Spot = SpotInput("Please enter your spot");
+                Console.WriteLine($"You just have entered {Spot}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+        }
+
+        public static string SpotInput(string inputMsg)
+        {
+            Console.WriteLine(inputMsg);
+            string output = Console.ReadLine();
+
+            if (output.Length != 2)
+            {
+                throw new ArgumentException("Invalid input, make sure you enter a valid position such as b2");
+            }
+            else if (!int.TryParse(output.Substring(1), out int gridPosX) || !char.TryParse(output.Substring(0, 1), out char gridPosY))
+            {
+                throw new ArgumentException("Invalid position, make sure your pos is correct");
+            }
+
+            return output;
+        }
+
+        public static void DisplayGrid()
+        {
+            //grid here
+        }
+    }
+}
