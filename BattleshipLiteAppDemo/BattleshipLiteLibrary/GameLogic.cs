1c2016417ee8411fe919d0cd6cb808361517fff8
lexicon06 <pablo.hehe2@gmail.com>
2024-01-16 08:21:28 -0300

    Logic and display mostly done

--- BattleshipLiteAppDemo/BattleshipLiteLibrary/GameLogic.cs
+++ BattleshipLiteAppDemo/BattleshipLiteLibrary/GameLogic.cs
@@ -64,9 +64,37 @@ namespace BattleshipLiteLibrary
                 throw new ArgumentException("Invalid position, make sure your pos is correct");
             }
 
-            //throw new NotImplementedException();
-
             return true;
+        }
+
+        public static bool PlayerStillActive(PlayerInfoModel opponent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static int GetShotCount(PlayerInfoModel winner)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static (string row, int column) SplitShotIntoRowAndColumn(string shot)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool ValidateShot(string row, int column)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool IdentifyShotResult(PlayerInfoModel opponent, string row, int column)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void MarkShotResult(PlayerInfoModel activePlayer, string row, int column, bool isAHit)
+        {
+            throw new NotImplementedException();
         }
     }
 }
--- BattleshipLiteAppDemo/ConsoleUI/Program.cs
+++ BattleshipLiteAppDemo/ConsoleUI/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 using BattleshipLiteLibrary;
@@ -23,18 +24,31 @@ namespace ConsoleUI
             {
                 DisplayShotGrid(activePlayer);
 
-                // Display grid from player 1 on where they fired
-                // Ask player 1 for a shot
-                // Determine if it is a valid shot
                 RecordPlayerShot(activePlayer, opponent);
-                // Determine shot results
-                // Determine if the game is over
-                // If over, set player1 as the winner 
-                // else swap positions (activePlayer to opponent)
-                if(winner!=null) Console.WriteLine(winner);
+
+                bool doesGameContinue = GameLogic.PlayerStillActive(opponent);
+
+                if (doesGameContinue)
+                {
+                    //swap pos
+                    (activePlayer, opponent) = (opponent, activePlayer);
+                }
+                else
+                {
+                    winner = activePlayer;
+                }
+
             } while (winner == null);
 
+            IdentifyWinner(winner);
+
             Console.ReadLine();
+        }
+
+        private static void IdentifyWinner(PlayerInfoModel winner)
+        {
+            Console.WriteLine($"Gratz to {winner.UsersName} for winning!");
+            Console.WriteLine($"{winner.UsersName} took {GameLogic.GetShotCount(winner)} shots.");
         }
 
         private static void WelcomeMessage()
@@ -118,85 +132,36 @@ namespace ConsoleUI
             }
         }
 
-        private static void RecordPlayerShot(PlayerInfoModel p1, PlayerInfoModel p2)
+        private static void RecordPlayerShot(PlayerInfoModel activePlayer, PlayerInfoModel opponent)
         {
-            // Ask for a shot ask for B2 or B and then 2
-            // Determine what row or colum that is - split it apart
-            // Determine if that a valid shot
-            // Go back to the begining if not a valid shot
+            bool isValidShot = false;
+            string row = "";
+            int column = 0;
+            do
+            {
+                string shot = AskForShot();
+                (row, column) = GameLogic.SplitShotIntoRowAndColumn(shot);
+                isValidShot = GameLogic.ValidateShot(row, column);
+
+                if (!isValidShot)
+                {
+                    Console.WriteLine("Please enter a valid shot.");
+                }
+
+
+            } while (!isValidShot);
+
+            bool isAHit = GameLogic.IdentifyShotResult(opponent, row, column);
+
+            GameLogic.MarkShotResult(activePlayer, row, column, isAHit);
+
         }
 
-        //static void Main(string[] args)
-        //{
-        //    WelcomePlayers();
-        //    bool KeepRunning = true;
-
-        //    do
-        //    {
-        //        try
-        //        {
-        //            string Spot = SpotInput("Please enter your spot");
-        //            Console.WriteLine($"You just have entered {Spot}");
-        //        }
-        //        catch (Exception ex)
-        //        {
-        //            Console.WriteLine(ex.Message);
-        //        }
-
-        //        try
-        //        {
-        //            KeepRunning = WannaPlay();
-
-        //        }
-        //        catch (Exception ex)
-        //        {
-        //            Console.WriteLine(ex.Message);
-        //        }
-
-        //    } while (KeepRunning);
-        //    Console.ReadLine();
-        //}
-
-        //public static string SpotInput(string inputMsg)
-        //{
-        //    Console.WriteLine(inputMsg);
-        //    string output = Console.ReadLine();
-
-        //    if (output.Length != 2)
-        //    {
-        //        throw new ArgumentException("Invalid input, make sure you enter a valid position such as b2");
-        //    }
-        //    else if (!char.IsLetter(output[0]) || !char.IsDigit(output[1]))
-        //    {
-        //        throw new ArgumentException("Invalid position, make sure your pos is correct");
-        //    }
-
-        //    return output;
-        //}
-
-        //private static void WelcomePlayers()
-        //{
-        //    Console.WriteLine("Battleship game v1.0 Console Game");
-        //}
-
-        //private static void DisplayGrid()
-        //{
-        //    //grid here
-        //}
-
-        //private static bool WannaPlay()
-        //{
-        //    string userInput = Console.ReadLine();
-        //    Console.WriteLine("Wanna play? yes/no");
-        //    if (userInput.ToLower() == "yes" || userInput.ToLower() == "no")
-        //    {
-
-        //        return userInput.ToLower() == "yes";
-        //    }
-        //    else
-        //    {
-        //        throw new InvalidDataException("Bad input the value must be only yes or no");
-        //    }
-        //}
+        private static string AskForShot()
+        {
+            Console.WriteLine($"Please enter your shot selection");
+            string output = Console.ReadLine();
+            return output;
+        }
     }
 }
