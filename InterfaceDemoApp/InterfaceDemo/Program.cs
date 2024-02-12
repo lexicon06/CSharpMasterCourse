d41d9137e4e74482ba9fed0b64c0e9938e9173ac
lexicon06 <pablo.hehe2@gmail.com>
2024-02-12 18:37:45 -0300

    interfaces are contracts use them as types when you need common functionality

---
+++ InterfaceDemoApp/InterfaceDemo/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceDemo
+{
+    //Interface is a contract
+    //Classes implement interfaces
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<IComputerController> controllers = new List<IComputerController>();
+            Keyboard keyboard = new Keyboard();
+            GameController gameController = new GameController();
+            BatteryPowerGameController battery = new BatteryPowerGameController();
+            BatteryPoweredKeyboard batteryPoweredkboard = new BatteryPoweredKeyboard();
+
+            controllers.Add(gameController);
+            controllers.Add(keyboard);
+            controllers.Add(battery);
+
+            foreach(IComputerController item in controllers)
+            {
+                if(item is BatteryPowerGameController joystick)
+                {
+                    Console.WriteLine(joystick.BatteryLevel);
+                }
+            }
+
+            using(GameController gc = new GameController())
+            {
+
+            }
+
+            List<IBatteryPowered> powereds = new List<IBatteryPowered>();
+            powereds.Add(battery);
+
+
+            Console.ReadLine();
+        }
+    }
+
+
+    public interface IComputerController : IDisposable
+    {
+        void Connect();
+        void CurrentKeyPressed();
+    }
+
+    public class Keyboard : IComputerController
+    {
+        public void Connect()
+        {
+        }
+        public void CurrentKeyPressed()
+        {
+        }
+
+        public string ConnectionType { get; set; }
+
+    }
+
+    public interface IBatteryPowered
+    {
+        int BatteryLevel { get; set;}
+    }
+
+    public class BatteryPoweredKeyboard:IBatteryPowered
+    {
+        public int BatteryLevel { get; set; }
+    }
+
+    public class GameController : IComputerController, IDisposable
+    {
+        public void Connect()
+        {
+
+        }
+
+        public void CurrentKeyPressed()
+        {
+
+        }
+
+        public void Dispose()
+        {
+
+        }
+    }
+
+
+    public class BatteryPowerGameController:GameController, IBatteryPowered
+    {
+        public int BatteryLevel { get; set; }
+
+    }
+}
