5f2c6afc7f5f7921e8b2f12a872858519f9c98e9
lexicon06 <pablo.hehe2@gmail.com>
2024-02-12 18:55:40 -0300

    Homework done, used interfaces and virtual void to later override when inherited classes

---
+++ InterfacesExerciseApp/InterfacesExercise/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<IPhone> phones = new List<IPhone>();
+            Alpha123 phone = new Alpha123();
+            Beta123 phone2 = new Beta123();
+
+            phones.Add(phone);
+            phones.Add(phone2);
+
+            foreach(var cel in phones) { 
+            
+                if(cel is Alpha123 wtf)
+                {
+                    Console.WriteLine(wtf.model);
+                }
+
+                if(cel is Beta123 newphone)
+                {
+                    Console.WriteLine(newphone.Number);
+                }
+
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+    public class Alpha123 : IPhone, IDisposable
+    {
+        public virtual string model { get; set; } = "Alpha";
+        public int serialNumber { get; set; }
+
+        public void Dispose()
+        {
+        }
+    }
+
+    public class Beta123 : Alpha123
+    {
+        public override string model { get; set; } = "Beta";
+        public int Number { get; set; } = 1234556;
+    }
+
+    public interface IPhone
+    {
+        string model { get; set; }
+        int serialNumber { get; set; }
+    }
+
+
+}
