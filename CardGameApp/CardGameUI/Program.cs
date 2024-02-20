11f768924997df242a3ec636aeb6862efaa1a8a3
lexicon06 <pablo.hehe2@gmail.com>
2024-02-20 17:52:36 -0300

    Created a project to practice random and another project to practice what i've just learned

--- CardGameApp/CardGameUI/Program.cs
+++ CardGameApp/CardGameUI/Program.cs
@@ -10,6 +10,74 @@ namespace CardGameUI
     {
         static void Main(string[] args)
         {
+            Console.ReadLine();
         }
+    }
+
+    public abstract class Deck
+    {
+        private List<PlayingCard> fullDeck = new List<PlayingCard>();
+        private List<PlayingCard> drawPile = new List<PlayingCard>();
+        private List<PlayingCard> discardPile = new List<PlayingCard>();
+        public void CreateDeck()
+        {
+            fullDeck.Clear();
+
+            for (int suit = 0; suit < 4; suit++)
+            {
+                for(int val = 0; val < 13; val++)
+                {
+                    fullDeck.Add(new PlayingCard { Suit = (CardSuit)suit, Value = (CardValue)val });
+                }
+            }
+        }
+
+        public virtual void SuffleDeck()
+        {
+            var rnd = new Random();
+            drawPile = fullDeck.OrderBy(x => rnd.Next()).ToList();
+
+
+        }
+
+        public abstract List<PlayingCard> DealCard();
+
+        public virtual PlayingCard RequestCard()
+        {
+            return null;
+        }
+
+    }
+
+
+    public class PlayingCard
+    {
+        public CardSuit Suit { get; set; }
+        public CardValue Value { get; set; }
+    }
+
+    public enum CardValue
+    {
+        Ace,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King
+    }
+
+    public enum CardSuit
+    {
+        Hearts,
+        Clubs,
+        Diamonds,
+        Spades
     }
 }
---
+++ ListExerciseApp/ListExercise/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            List<string> list = new List<string>();
+            list.Add("1 Hola");
+            list.Add("2 Adios");
+            list.Add("3 Hello");
+            list.Add("4 Goodbye");
+
+            var random = new Random();
+
+            List<string> nuevaLista = list.OrderBy(x => random.Next()).ToList();
+
+            foreach(var item in nuevaLista)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine(); 
+        }
+    }
+}
