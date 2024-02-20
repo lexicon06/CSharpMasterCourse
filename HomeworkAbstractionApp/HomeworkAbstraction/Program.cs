3ac51124a7777ecd7817b7f92c03c5b7a1c3fdd4
lexicon06 <pablo.hehe2@gmail.com>
2024-02-20 15:56:03 -0300

    Some homework done and now mini project using different type of classes

---
+++ HomeworkAbstractionApp/HomeworkAbstraction/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkAbstraction
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+
+            Tesla _tesla = new Tesla();
+
+            Console.WriteLine(_tesla.Model); 
+            Console.WriteLine(_tesla.Name);
+
+            TeslaItem _teslaItem = new TeslaItem();
+
+            Console.WriteLine(_teslaItem.Year);
+
+            Console.ReadLine();
+        }
+    }
+
+    public abstract class Inventory
+    {
+        string ProductType { get; set; }
+        int PartNumber { get; set; }
+    }
+
+    public interface ICar
+    {
+        string Name { get; set; }
+        string Model { get; set; }
+        int Chasis_SN { get; set; }
+
+    }
+
+    public class Tesla : ICar
+    {
+        public string Name { get; set; } = "Electric Tesla";
+        public string Model { get; set; } = "3401 Modulus";
+        public int Chasis_SN { get; set; } = 240652;
+        public string ProductType { get; set; }
+        public int PartNumber { get; set; }
+    }
+
+    public class TeslaItem : Inventory
+    {
+        public int Year { get; set; } = 2024;
+    }
+}
+
+
+
---
+++ HomeworkMethodOverrideApp/HomeworkMethodOverride/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkMethodOverride
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person p1 = new Person();
+            p1.Name = "Will";
+            PersonBase p2 = new PersonBase();
+            p2.Name = "Richard";
+
+            p1.Cook();
+            p2.Cook();
+
+            Console.ReadLine();
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public virtual void Cook()
+        {
+            Console.WriteLine($"{Name} makes pasta");
+        }
+    }
+
+    public class PersonBase : Person
+    {
+        public override void Cook()
+        {
+            //base.Cook();
+
+            Console.WriteLine($"{Name} makes salad");
+        }
+    }
+}
