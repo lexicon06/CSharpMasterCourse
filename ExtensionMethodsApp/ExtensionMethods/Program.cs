69a2a16f45a80f0bdcb73c898b2843bb96c490b3
lexicon06 <pablo.hehe2@gmail.com>
2024-03-30 07:30:39 -0300

    Homework ok

---
+++ ExtensionMethodsApp/ExtensionMethods/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+
+            p.SetDefaultAge().PrintInfo();
+
+            Console.ReadLine();
+        }
+    }
+
+    public class Person
+    {
+        public string PersonName { get; set; }
+        public int PersonAge { get; set; }
+    }
+
+
+    public static class PersonHandler {
+        public static Person SetDefaultAge(this Person p)
+        {
+            p.PersonAge = 31;
+            return p;
+        }
+
+
+        public static Person PrintInfo(this Person p)
+        {
+            Console.WriteLine($"The person has {p.PersonAge} yo");
+
+            return p;
+        }
+    }
+
+}
